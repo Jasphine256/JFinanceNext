763cedaba73e018d37502670d01d2f76a86dcf02
Jasphine2020 <jasphinesoftware@gmail.com>
2024-11-11 17:41:25 +0300

    added homepage banner

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import PackageRow from "@/components/compositions/Packages";
+import Banner from "@/components/widgets/Banner";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-start justify-start p-1 sm:p-4">
+      <Banner/>
       <PackageRow/>
       <PackageRow/>
     </div>
