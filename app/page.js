ee275eb859db9be3686f30cfc4155e016c15481d
Jasphine2020 <jasphinesoftware@gmail.com>
2024-11-12 14:53:17 +0300

    added some dynamics to pages and improved the design

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
-import PackageRow from "@/components/compositions/Packages";
+import PackageRow from "@/components/Packages";
 import Banner from "@/components/widgets/Banner";
 
 export default function Home() {
   return (
     <main className="w-full flex flex-col items-start justify-start p-1 sm:p-4 mt-16">
       <Banner text={"Welcome Mikhael"} balance={"200,000"}/>
-      <PackageRow/>
-      <PackageRow/>
+      <PackageRow title="Loan Packages" type="loans"/>
+      <PackageRow title="Saving Packages" type="savings"/>
     </main>
   );
 }
---
+++ components/Packages.jsx
@@ -0,0 +1,95 @@
+import Link from "next/link"
+const packages = [
+  {
+    id: 1,
+    name: 'BASIC PACKAGE',
+    imageSrc: '/images/package image.png',
+    imageAlt: "loan packege.png",
+    amount: '100,000',
+    return: '110,000',
+    days: '30 days',
+  },
+  {
+    id: 2,
+    name: 'BASIC PACKAGE',
+    imageSrc: '/images/package image.png',
+    imageAlt: "loan packege.png",
+    amount: '100,000',
+    return: '110,000',
+    days: '30 days',
+  },
+  {
+    id: 3,
+    name: 'BASIC PACKAGE',
+    imageSrc: '/images/package image.png',
+    imageAlt: "loan packege.png",
+    amount: '100,000',
+    return: '110,000',
+    days: '30 days',
+  },
+  {
+    id: 4,
+    name: 'BASIC PACKAGE',
+    imageSrc: '/images/package image.png',
+    imageAlt: "loan packege.png",
+    amount: '100,000',
+    return: '110,000',
+    days: '30 days',
+  },
+  {
+    id: 5,
+    name: 'BASIC PACKAGE',
+    imageSrc: '/images/package image.png',
+    imageAlt: "loan packege.png",
+    amount: '100,000',
+    return: '110,000',
+    days: '30 days',
+  },
+  {
+    id: 6,
+    name: 'BASIC PACKAGE',
+    imageSrc: '/images/package image.png',
+    imageAlt: "loan packege.png",
+    amount: '100,000',
+    return: '110,000',
+    days: '30 days',
+  },
+]
+
+export default function PackageRow(props) {
+  return (
+    <div className="w-full">
+      <div className="mx-auto max-w-2xl p-4 sm:px-6 sm:p-4 lg:max-w-7xl lg:p-4">
+        <h2 className="text-2xl font-bold tracking-tight">{props.title}</h2>
+
+        <div className="mt-6 flex flex-row items-start justify-start gap-x-6 gap-y-10 overflow-x-scroll scrollbar">
+          {packages.map((item) => (
+            <Link href={'/packages/view'} key={item.id} className="min-w-[300px] max-w-[500px]">
+              <div className="group relative w-full">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-zinc-900 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img
+                    alt={item.imageAlt}
+                    src={"/images/loan packege.png"}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                </div>
+                <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-md font-bold text-white">{item.name}</h3>
+                      <p className="mt-1 text-sm text-white">{item.days}</p>
+                    </div>
+                    <div className="flex flex-col items-end">
+                      <h3 className="text-sm text-green-500 font-bold">{item.amount}</h3>
+                      <p className="mt-1 text-sm text-blue-500 font-bold">RETURNING {item.return}</p>
+                    </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+  
